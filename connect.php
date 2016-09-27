0a55a56d60327f1811baf4635cf78157afa6e1cc
luca <luca.carabetta@gmail.com>
2016-09-27 09:56:45 +0200

    first commit

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+	$host = "localhost";
+	$user = "root";
+	$pass = "washomatic";	
+	mysql_connect($host, $user, $pass) or die("Error establishing a database connection: " . mysql_error());
+?>
